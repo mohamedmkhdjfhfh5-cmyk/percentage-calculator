6bd17ea00bd9ea83e24a7f35c6474ff670f7661b
Mohamed El-Sayed <mohamedmkhdjfhfh5@gmail.com>
2026-07-18 02:28:56 +0300

    Add Percentage Calculator
    Initial upload of the project.

---
+++ percentage_calculator.py
@@ -0,0 +1,6 @@
+student_score = float(input("Enter student score: "))
+total_score = float(input("Enter total score: "))
+
+percentage = (student_score / total_score) * 100
+
+print("Percentage =", percentage, "%")
